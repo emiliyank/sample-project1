a807da901ab72c77a8976bec5bea2f8eca4b32e1
Emiliyan <emiliyan.kadiyski@shkolo.com>
2021-06-24 19:01:01 +0300

    Initial commit for sample project

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Main {
+    public static void odd(){
+        int[] a  = new int[40];
+        int[] a ={11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99};
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
